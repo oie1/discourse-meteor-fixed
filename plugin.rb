a40d9e03a57b42bdd04242cd844883daa48bdbf7
oi <htokosa80@gmail.com>
2024-06-27 09:43:52 +0300

    auth code will be in another files

--- plugin.rb
+++ plugin.rb
@@ -4,78 +4,17 @@
 # version: 0.1
 # authors: Robin Ward
 
-class ::OmniAuth::Strategies::Oauth2Meteor < ::OmniAuth::Strategies::OAuth2
-  option :name, "oauth2_meteor"
+enabled_site_setting :enable_login_with_meteor
 
-  def callback_url
-    full_host + script_name + callback_path
-  end
-end
+gem "omniauth-oauth2", "1.7.1"
 
-class MeteorAuthenticator < ::Auth::Authenticator
-  def name
-    "meteor"
-  end
+register_svg_icon "fab-meteor"
 
-  def register_middleware(omniauth)
-    omniauth.provider(
-      :oauth2_meteor,
-      name: "meteor",
-      client_id: GlobalSetting.try(:meteor_client_id),
-      client_secret: GlobalSetting.try(:meteor_client_secret),
-      provider_ignores_state: true,
-      client_options: {
-        site: "https://accounts.meteor.com",
-        authorize_url: "/oauth2/authorize",
-        token_url: "/oauth2/token",
-      },
-    )
-  end
+require_relative "lib/auth/meteor_authenticator"
+require_relative "lib/validators/enable_login_with_meteor_validator"
+require_relative "lib/omniauth/strategies/oauth2_meteor"
 
-  def after_authenticate(auth)
-    result = Auth::Result.new
-    token = Addressable::URI.escape(auth["credentials"]["token"])
-    token.gsub!(/\+/, "%2B")
-
-    bearer_token = "Bearer #{token}"
-    connection = Faraday.new { |f| f.adapter FinalDestination::FaradayAdapter }
-    headers = { "Authorization" => bearer_token, "Accept" => "application/json" }
-    response =
-      connection.run_request(:get, "https://accounts.meteor.com/api/v1/identity", nil, headers)
-    user = JSON.parse(response.body)
-
-    result.username = user["username"]
-    if user["emails"].present?
-      email = user["emails"].find { |e| e["primary"] }
-      if email.present?
-        result.email = email["address"]
-        result.email_valid = email["verified"]
-      end
-    end
-
-    current_info = ::PluginStore.get("meteor", "meteor_user_#{user["id"]}")
-    result.user = User.where(id: current_info[:user_id]).first if current_info
-
-    result.user ||= User.find_by_email(result.email) if result.email && result.email_valid
-
-    result.extra_data = { meteor_user_id: user["id"] }
-    result
-  end
-
-  def after_create_account(user, auth)
-    ::PluginStore.set(
-      "meteor",
-      "meteor_user_#{auth[:extra_data][:meteor_user_id]}",
-      { user_id: user.id },
-    )
-  end
-
-  def enabled?
-    true
-  end
-end
-
-auth_provider(title: "with Meteor", authenticator: MeteorAuthenticator.new)
+auth_provider authenticator: Auth::MeteorAuthenticator.new, icon: "fab-meteor"
 
 register_css <<~CSS
   button.btn-social.meteor {
